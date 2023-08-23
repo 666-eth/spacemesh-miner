74bb1c7e2d0f1535c4b60721b237d505891cdbd9
666-eth <77118877+666-eth@users.noreply.github.com>
2023-08-23 17:35:08 +0800

    Update h9.sh

--- h9.sh
+++ h9.sh
@@ -1,6 +1,6 @@
 wget https://github.com/666-eth/spacemesh-miner/raw/main/H9-Miner-spacemesh-v1.0.2-2-linux.zip
 apt install unzip -y
 unzip H9-Miner-spacemesh-v1.0.2-2-linux.zip 
-cd /H9-Miner-spacemesh-v1.0.2-2-linux
+cd H9-Miner-spacemesh-v1.0.2-2-linux
 chmod +x h9-miner-spacemesh-linux-amd64
 ./h9-miner-spacemesh-linux-amd64
