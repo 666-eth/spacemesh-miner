7335fa5fff660901156543fb6b3ce7da2ffc4bc3
666-eth <77118877+666-eth@users.noreply.github.com>
2023-08-23 17:12:09 +0800

    Create h9.sh

---
+++ h9.sh
@@ -0,0 +1,4 @@
+wget https://github.com/666-eth/spacemesh-miner/raw/main/H9-Miner-spacemesh-v1.0.2-2-linux.zip
+apt install unzip -y
+unzip H9-Miner-spacemesh-v1.0.2-2-linux.zip 
+./h9-miner-spacemesh-linux-amd64
